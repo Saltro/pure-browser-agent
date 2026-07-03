6c95dc2bf9c92dc0984b20d6f3f093091b5005e2
Saltro <2373117747@qq.com>
2026-07-03 15:37:16 +0800

    fix: remove dev allowed server hosts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
     headers: {
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Opener-Policy': 'same-origin'
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
     },
-    allowedHosts: ['workspace-wsd221d4daa0c3c571-http-5173.ide.byted.org'],
-  }
+  },
 });
